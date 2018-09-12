aa6a2ed8d8a1420ad05a8520090caf54265074c4
objectorange <objectorange@protonmail.com>
2018-09-11 21:55:37 -0400

    use more abstract model

--- src/main/java/io/onemfive/neo4j/GraphUtil.java
+++ src/main/java/io/onemfive/neo4j/GraphUtil.java
@@ -1,16 +1,16 @@
 package io.onemfive.neo4j;
 
-import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.PropertyContainer;
 
 import java.util.Map;
 import java.util.Set;
 
 public class GraphUtil {
 
-    public static void updateProperties(Node node, Map<String,Object> attributes) {
+    public static void updateProperties(PropertyContainer container, Map<String,Object> attributes) {
         Set<String> keys = attributes.keySet();
         for(String key : keys) {
-            node.setProperty(key,attributes.get(key));
+            container.setProperty(key,attributes.get(key));
         }
     }
 
