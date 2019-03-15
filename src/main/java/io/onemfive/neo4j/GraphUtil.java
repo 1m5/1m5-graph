d4d5b9b1670b29e4fcdee1f0039baee6116eaed9
objectorange <objectorange@protonmail.com>
2019-03-15 11:19:04 -0400

    network peer helpers

--- src/main/java/io/onemfive/neo4j/GraphUtil.java
+++ src/main/java/io/onemfive/neo4j/GraphUtil.java
@@ -1,5 +1,6 @@
 package io.onemfive.neo4j;
 
+import io.onemfive.data.NetworkPeer;
 import org.neo4j.graphdb.PropertyContainer;
 
 import java.util.Map;
@@ -12,6 +13,16 @@ public class GraphUtil {
         for(String key : keys) {
             container.setProperty(key,attributes.get(key));
         }
+    }
+
+    public static NetworkPeer mapToPeer(Map<String,Object> m) {
+        NetworkPeer p = new NetworkPeer();
+        p.fromMap(m);
+        return p;
+    }
+
+    public static Map<String,Object> peerToMap(NetworkPeer p) {
+        return p.toMap();
     }
 
 }
