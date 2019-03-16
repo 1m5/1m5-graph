bd4def8948be505845d2c0304e57613dcc60e126
objectorange <objectorange@protonmail.com>
2019-03-16 15:12:52 -0400

    add getAttributes helper

--- src/main/java/io/onemfive/neo4j/GraphUtil.java
+++ src/main/java/io/onemfive/neo4j/GraphUtil.java
@@ -2,16 +2,25 @@ package io.onemfive.neo4j;
 
 import org.neo4j.graphdb.*;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class GraphUtil {
 
-    public static void updateProperties(PropertyContainer container, Map<String,Object> attributes) {
-        Set<String> keys = attributes.keySet();
+    public static void updateProperties(PropertyContainer c, Map<String,Object> a) {
+        Set<String> keys = a.keySet();
         for(String key : keys) {
-            container.setProperty(key,attributes.get(key));
+            c.setProperty(key,a.get(key));
         }
+    }
+
+    public static Map<String,Object> getAttributes(PropertyContainer c) {
+        Map<String,Object> a = new HashMap<>();
+        for (String key : c.getPropertyKeys()) {
+            a.put(key, c.getProperty(key));
+        }
+        return a;
     }
 
 }
